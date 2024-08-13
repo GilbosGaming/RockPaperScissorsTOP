018f3c77a4e57ca8687b2ddf6a095d3b7ad30ecf
GilbosGaming <hgilbs21@gmail.com>
2024-08-13 21:12:53 +0100

    added dom to show score and results

--- script.js
+++ script.js
@@ -1,89 +1,69 @@
+let rockButton = document.getElementById("rock"); 
+let paperButton = document.getElementById("paper");
+let scissorsButton = document.getElementById("scissors");
+const resultDiv = document.querySelector('.result');
+const yourScoreDiv = document.getElementById('yourScore');
+const cpuScoreDiv = document.getElementById('cpuScore');
+
+
 let humanScore = 0;
 let computerScore = 0;
 
+function updateScore() {
+    yourScoreDiv.textContent = humanScore;
+    cpuScoreDiv.textContent = computerScore;
+}
+
 function getComputerChoice() {
-    let cChoice = Math.random();
-
-        if (cChoice < 1 / 3){
-            return "scissors";
-        }
-        else if(cChoice > 1 / 3 && cChoice < 2 / 3){
-            return "rock";
-        }
-        else{
-            return "paper";
-        }
-
-    }
-
-function getHumanChoice() {
-    let hChoice = window.prompt("Choose rock, paper, or scissors").toLowerCase();
-
-    if (hChoice === "rock"){
-        return "rock";
-    }
-    else if (hChoice === "paper"){
-        return "paper";
-    }
-    else if (hChoice === "scissors"){
-        return "scissors";
-    }
-    else{
-        return "That is not a valid input";
-    }
-
-}
-
-let humanSelection = getHumanChoice();
-let computerSelection = getComputerChoice();
-
-
-function playRound(hChoice, cChoice) {
     
-    if ((humanSelection) === "rock" && (computerSelection) === "scissors" || (humanSelection) === "paper" && (computerSelection) === "rock" || (humanSelection) === "scissors" && (computerSelection) === "paper"){
+    const cChoices = ['rock', 'paper', 'scissors'];
+    const randomChoice = Math.floor(Math.random() * cChoices.length);
+    return cChoices[randomChoice];
+
+    }
+
+function playRound(humanSelection, computerSelection) {
+    
+    let result;
+    if (humanSelection === computerSelection) {
+        result = "It's a tie!";
+    }    
+    else if ((humanSelection) === "rock" && (computerSelection) === "scissors" || (humanSelection) === "paper" && (computerSelection) === "rock" || (humanSelection) === "scissors" && (computerSelection) === "paper"){
         humanScore++; 
+        result = `You win! ${humanSelection} beats ${computerSelection}`;
         
     }
-    else if ((humanSelection) === "rock" && (computerSelection) === "paper" || (humanSelection) === "paper" && (computerSelection) === "scissors" || (humanSelection) === "scissors" && (computerSelection) === "rock"){
+    else {
         computerScore++; 
-        
-    }
-    else {
-        return 0;
+        result = `You lose ${computerSelection} beats ${humanSelection} womp, womp.`;
     }
 
-
+    updateScore();
+    return result;
+    
 }
-
-function playGame(){
    
-    for (let i = 0; i < 4; i++) {
-    let humanSelection = getHumanChoice();
-    let computerSelection = getComputerChoice();
-    if (humanSelection === "That is not a valid input") {
-        i--;
-        }
 
-    playRound(humanSelection, computerSelection)
+rockButton.addEventListener("click", () => {
+    const computerSelection = getComputerChoice();
+    const humanSelection = 'rock'
+    const result = playRound(humanSelection, computerSelection);
+    resultDiv.textContent = result;
+});
 
-    }
+paperButton.addEventListener("click", () => {
+    const computerSelection = getComputerChoice();
+    const humanSelection = 'paper'
+    const result = playRound(humanSelection, computerSelection);
+    resultDiv.textContent = result;
+});
 
-    if (humanScore > computerScore) {
-        console.log("You Win!");
-    }
-    else if (computerScore > humanScore){
-        console.log("Womp, Womp...You suck!");
-    }
-    else {
-        console.log("It's a tie")
-    }
-}
-
-playGame();
-
-
-
-
+scissorsButton.addEventListener("click", () => {
+    const computerSelection = getComputerChoice();
+    const humanSelection = 'scissors'
+    const result = playRound(humanSelection, computerSelection);
+    resultDiv.textContent = result;
+});
 
 
 
